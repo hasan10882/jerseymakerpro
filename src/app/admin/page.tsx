df11bedf791e365ddb690d543ec6a83610e3b328
Hasan Khan <hasan10882@gmail.com>
2025-05-06 14:42:51 +0600

    Fix ESLint issues for Vercel

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { auth } from "../../firebaseConfig";
 import { onAuthStateChanged, getIdTokenResult } from "firebase/auth";
 
 export default function AdminDashboard() {
