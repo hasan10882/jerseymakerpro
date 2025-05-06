69751191341fcc1a3f1b32f7ce0b6658df21b11a
Hasan Khan <hasan10882@gmail.com>
2025-05-06 14:06:15 +0600

    Disable ESLint rules for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true // ✅ Skip ESLint during Vercel build
+    ignoreDuringBuilds: true
   }
 };
 
